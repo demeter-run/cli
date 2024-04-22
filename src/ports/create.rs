2f58c62eb62941e3e1d72be881a8281aca1c3dab
Alejandro Avagnina <avatxus@gmail.com>
2024-04-22 06:17:08 -0300

    chore: show better option info (#29)

--- src/ports/create.rs
+++ src/ports/create.rs
@@ -19,6 +19,7 @@ pub async fn run(_args: Args, cli: &crate::Cli) -> miette::Result<()> {
         .into_diagnostic()?;
     loop {
         let kind = inquire::Select::new("Choose the port kind", kind_options.clone())
+            .with_page_size(kind_options.len())
             .prompt()
             .into_diagnostic()?;
 
@@ -26,30 +27,44 @@ pub async fn run(_args: Args, cli: &crate::Cli) -> miette::Result<()> {
             .await
             .into_diagnostic()?;
 
-        let network_options = options.networks.clone();
+        let network_options = options.get_networks();
 
-        let network = inquire::Select::new("Choose the network", network_options)
+        let selected_network = inquire::Select::new("Choose the network", network_options)
             .prompt()
             .into_diagnostic()?;
 
-        let mut version = String::new();
-        let network_versions = options.get_network_versions(&network);
+        let payload_network = options
+            .find_network_key_by_value(&selected_network)
+            .unwrap();
+
+        // versions could be empty. If so, skip the version selection
+        let mut selected_version = String::new();
+        let mut payload_version = String::new();
+        let network_versions = options.get_network_versions(&payload_network);
         if !network_versions.is_empty() {
-            version = inquire::Select::new("Choose the version", network_versions)
+            selected_version = inquire::Select::new("Choose the version", network_versions)
                 .prompt()
                 .into_diagnostic()?;
+            payload_version = options
+                .find_version_label_by_number(&payload_network, &selected_version)
+                .unwrap();
         }
 
-        let tier = inquire::Select::new("Choose the throughput tier", options.tiers)
+        let tier_options = options.get_tiers();
+
+        let selected_tier = inquire::Select::new("Choose the throughput tier", tier_options)
             .prompt()
             .into_diagnostic()?;
+
+        let payload_tier: String = options.find_tier_key_by_value(&selected_tier).unwrap();
+
         println!("You are about to create a new port with the following configuration:");
         println!("Kind: {}", kind);
-        println!("Network: {}", network);
-        if !version.is_empty() {
-            println!("Version: {}", version);
+        println!("Network: {}", selected_network);
+        if !selected_version.is_empty() {
+            println!("Version: {}", selected_version);
         }
-        println!("Tier: {}", tier);
+        println!("Tier: {}", selected_tier);
 
         let confirm = inquire::Confirm::new("Do you want to proceed?")
             .prompt()
@@ -60,9 +75,15 @@ pub async fn run(_args: Args, cli: &crate::Cli) -> miette::Result<()> {
             return Ok(());
         }
 
-        let result = api::create_port(cli, &kind, &network, &version, &tier)
-            .await
-            .into_diagnostic()?;
+        let result = api::create_port(
+            cli,
+            &kind,
+            &payload_network,
+            &payload_version,
+            &payload_tier,
+        )
+        .await
+        .into_diagnostic()?;
 
         pretty_print_port(result);
 
