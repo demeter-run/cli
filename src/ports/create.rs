a58cc5ce8b4e422820131551671015f358092d14
Alexsander Falcucci <alex.falcucci@gmail.com>
2024-08-02 15:02:02 +0200

    refactor: refactor code to use variables and pass references consistently
    - update the code to use the `kinds` variable instead of calling the `map` function on `kind_options`
    - update the code to pass `&kind` instead of `kind` when calling the `api::get_public` function
    - update the code to use the `network_options` variable when calling the `inquire::select` constructor
    - update the code to use the `payload_network` variable when calling the `options.get_network_versions` function
    - update the code to use the `network_versions` variable when checking if it is empty
    - update the code to pass `&payload_network` and `&selected_version` when calling the `options.find_version_label_by_number` function
    - update the code to use the `tier_options` variable when calling the `inquire::select` constructor
    - update the code to pass `&selected_tier` when calling the `options.find_tier_key_by_value` function
    - update the code to print the `selected_network`, `selected_version`, and `selected_tier` variables instead of the commented out lines in `println!` statements
    - change the code to pass `&kind` when calling the `rpc::resources::create` function

--- src/ports/create.rs
+++ src/ports/create.rs
@@ -20,58 +20,59 @@ pub async fn run(_args: Args, cli: &crate::Cli) -> miette::Result<()> {
     let (access_token, _, id, _) = extract_context_data(cli);
     let kind_options: Vec<Resource> = rpc::resources::find(&access_token, &id).await?;
 
-    // let kind_options: Vec<String> = api::get(cli, "ports?listOnly=true")
-    //     .await
-    //     .into_diagnostic()?;
+    let kinds = kind_options
+        .iter()
+        .map(|x| x.kind.clone())
+        .collect::<Vec<String>>();
 
-    let kind = inquire::Select::new("Choose the port kind", Vec::from(["CardanoNodePort"]))
+    let kind = inquire::Select::new("Choose the port kind", kinds)
         .with_page_size(1)
         .prompt()
         .into_diagnostic()?;
 
-    // let options: PortOptions = api::get_public(&format!("metadata/ports/{}",
-    // kind))     .await
-    //     .into_diagnostic()?;
-    //
-    // let network_options = options.get_networks();
-    //
-    // let selected_network = inquire::Select::new("Choose the network",
-    // network_options)     .prompt()
-    //     .into_diagnostic()?;
-    //
-    // let payload_network = options
-    //     .find_network_key_by_value(&selected_network)
-    //     .unwrap();
+    let options: PortOptions = api::get_public(&format!("metadata/ports/{}", kind))
+        .await
+        .into_diagnostic()?;
+
+    let network_options = options.get_networks();
+
+    let selected_network = inquire::Select::new("Choose the network", network_options)
+        .prompt()
+        .into_diagnostic()?;
+
+    let payload_network = options
+        .find_network_key_by_value(&selected_network)
+        .unwrap();
 
     // versions could be empty. If so, skip the version selection
-    // let mut selected_version = String::new();
-    // let mut payload_version = String::new();
-    // let network_versions = options.get_network_versions(&payload_network);
-    // if !network_versions.is_empty() {
-    //     selected_version = inquire::Select::new("Choose the version",
-    // network_versions)         .prompt()
-    //         .into_diagnostic()?;
-    //     payload_version = options
-    //         .find_version_label_by_number(&payload_network, &selected_version)
-    //         .unwrap();
-    // }
+    let mut selected_version = String::new();
+    let mut payload_version = String::new();
+    let network_versions = options.get_network_versions(&payload_network);
+    if !network_versions.is_empty() {
+        selected_version = inquire::Select::new("Choose the version", network_versions)
+            .prompt()
+            .into_diagnostic()?;
 
-    // let tier_options = options.get_tiers();
+        payload_version = options
+            .find_version_label_by_number(&payload_network, &selected_version)
+            .unwrap();
+    }
 
-    // let selected_tier = inquire::Select::new("Choose the throughput tier",
-    // tier_options)     .prompt()
-    //     .into_diagnostic()?;
-    //
-    // let payload_tier: String =
-    // options.find_tier_key_by_value(&selected_tier).unwrap();
-    //
+    let tier_options = options.get_tiers();
+
+    let selected_tier = inquire::Select::new("Choose the throughput tier", tier_options)
+        .prompt()
+        .into_diagnostic()?;
+
+    let payload_tier: String = options.find_tier_key_by_value(&selected_tier).unwrap();
+
     println!("You are about to create a new port with the following configuration:");
     println!("Kind: {}", kind);
-    // println!("Network: {}", selected_network);
-    // if !selected_version.is_empty() {
-    //     println!("Version: {}", selected_version);
-    // }
-    // println!("Tier: {}", selected_tier);
+    println!("Network: {}", selected_network);
+    if !selected_version.is_empty() {
+        println!("Version: {}", selected_version);
+    }
+    println!("Tier: {}", selected_tier);
 
     let confirm = inquire::Confirm::new("Do you want to proceed?")
         .prompt()
@@ -82,7 +83,7 @@ pub async fn run(_args: Args, cli: &crate::Cli) -> miette::Result<()> {
         return Ok(());
     }
 
-    let result = rpc::resources::create(&access_token, &id, kind).await?;
+    let result = rpc::resources::create(&access_token, &id, &kind).await?;
 
     // pretty_print_port(result);
     pretty_print_ports_table(Vec::from([result]));
